df03ebc1db1f3a570ecfbe90e3c0b7ed71093855
Rob Martin <rmartin3@alumni.unca.edu>
2019-10-26 23:44:23 -0700

    Updates

---
+++ precache-manifest.a099c5770bb203ac714070cbd66bb71c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f229df87ea743dfabc8b5b06c3c5ae78",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "da897496e12b000d30a8",
+    "url": "/portfolio/static/css/main.4ad03e9e.chunk.css"
+  },
+  {
+    "revision": "9bc540d73c1fb69ddca4",
+    "url": "/portfolio/static/js/2.07a61cbf.chunk.js"
+  },
+  {
+    "revision": "da897496e12b000d30a8",
+    "url": "/portfolio/static/js/main.ee9a5fbb.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f0c031de8d60999f719ca256ece2cb19.js"
+  "/portfolio/precache-manifest.a099c5770bb203ac714070cbd66bb71c.js"
 );
 
 self.addEventListener('message', (event) => {
